76e4b9978ab2b78477849dce66b6def9ee2eda66
Vaishnavi patil <patil12vaishnavi@gmail.com>
2022-11-16 11:06:29 +0530

    added

--- Feature/OrangeHRM.py
+++ Feature/OrangeHRM.py
@@ -7,7 +7,7 @@ from selenium.webdriver.common.by import By
 
 @given('launch chrome browser')
 def launchBrowser(context):
-    context.serv_obj=Service("C:\web drivers selenium\chromedriver_win32\chromedriver.exe")
+    context.serv_obj=Service("C:\web drivers selenium\chromedriver_win32 (1)\chromedriver.exe")
     context.Driver=webdriver.Chrome(service=context.serv_obj)
 
 @when('Open orange HRM homepage')
@@ -20,7 +20,7 @@ def OpenHomepage(context):
 def verifylogo(context):
     status=context.Driver.find_element(By.XPATH,"//img[@alt='company-branding']").is_displayed()
     assert status is True
-
+print("vaishnavi")
 
 
 @Then('close Browser')
