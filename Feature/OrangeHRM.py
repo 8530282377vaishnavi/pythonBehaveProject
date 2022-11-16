e21ab22e15c9bff85db9926c0a798997b9679847
8530282377vaishnavi <115994508+8530282377vaishnavi@users.noreply.github.com>
2022-11-16 11:41:35 +0530

    Update OrangeHRM.py

--- Feature/OrangeHRM.py
+++ Feature/OrangeHRM.py
@@ -3,6 +3,7 @@ import selenium
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+dict2={"b":3}
 
 
 @given('launch chrome browser')
